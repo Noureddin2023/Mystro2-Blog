feec807cc84b2f2ee3b24659681fec9c327c14a9
Noureddin2023 <nam2021sa@gmail.com>
2023-01-04 17:12:15 +0100

    add Forms

--- myblog/settings.py
+++ myblog/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'taggit',
     'django_summernote',
     'posts',
+    'postcbv',
     
     ]   
     
@@ -60,7 +61,7 @@ ROOT_URLCONF = 'myblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
