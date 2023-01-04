c8713ad8a6df83a13bd0b0a2e6f3f7e78c17592d
Noureddin2023 <nam2021sa@gmail.com>
2023-01-04 01:10:53 +0100

    add GRUD Operations

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
@@ -11,3 +12,38 @@ def post_list(request):
 def post_detail(request,id):
     post = Post.objects.get(id=id)
     return render(request,'single.html',{'data':post})
+
+
+def create_post(request):
+    if request.method == 'POST':
+        print( 'in post...')
+        form = PostForm(request.POST,request.FILES)
+        if form.is_valid():
+            myform = form.save(commit=False)
+            myform.author = request.user
+            myform.save()
+    else:
+        print('in get ....')
+        form = PostForm()
+    
+    return render(request,'create.html',{'form':form})
+
+def edit_post(request,id):
+    post = Post.objects.get(id=id)
+    if request.method == 'POST':
+         print( 'in post...')
+         form = PostForm(request.POST,request.FILES,instance=post)
+         if form.is_valid():
+             myform = form.save(commit=False)
+             myform.author = request.user
+             myform.save()
+    else:
+        print('in get ....')
+        form = PostForm(instance=post)
+    
+    return render(request,'edit.html',{'form':form})
+
+def delete_post(request,id):
+    post = Post.objects.get(id=id)
+    post.delete()
+    return redirect('/blog')
