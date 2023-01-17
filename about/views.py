6f1d46b72e59bfe80372d8cb03ba239c132c508f
Noureddin2023 <nam2021sa@gmail.com>
2023-01-17 20:22:03 +0100

    add projects

--- about/views.py
+++ about/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import About , Skills , Education , Experience
+from .models import About , Skills , Education , Experience , Service , Projects
 # Create your views here.
 def home(request):
     about = About.objects.last()
@@ -7,12 +7,16 @@ def home(request):
     design_skills = Skills.objects.filter(type='Design')
     education = Education.objects.all()
     experience = Experience.objects.all()
+    services = Service.objects.all()
+    projects = Projects.objects.all()
 
 
     return render(request,'home.html',{'about':about,
        'coding_skills' : coding_skills,
        'design_skills' : design_skills,
        'education' : education,
-       'experience' : experience
+       'experience' : experience,
+       'services' : services,
+       'projects' : projects
 
        })
